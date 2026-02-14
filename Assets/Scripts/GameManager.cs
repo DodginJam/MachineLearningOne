20715743bc3cf03e9c423f0ec40b928f2253b472
DodginJam <157243206+DodginJam@users.noreply.github.com>
2026-02-14 23:51:10 +0000

    Completed the minimax implementation with notation and depth limit for performance. Can choose the amount of tiles required for victory.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -16,6 +16,10 @@ public class GameManager : MonoBehaviour
     public int Length
     { get; private set; } = 3;
 
+    [field: SerializeField, Min(3)]
+    public int WinningLineAmount
+    { get; private set; } = 3;
+
     [field: SerializeField]
     public float Spacing
     { get; private set; } = 1f;
@@ -28,6 +32,10 @@ public class GameManager : MonoBehaviour
     { get; private set; }
 
     public event Action<bool> TilesInteractionaStatusEvent;
+
+    [field: SerializeField, Min(1)]
+    public int DepthLimit_AI
+    { get; private set; } = 4;
 
     private void Start()
     {
@@ -68,11 +76,11 @@ public class GameManager : MonoBehaviour
                 {
                     for (int j = 0; j < Tiles.GetLength(1); j++)
                     {
-                        Tile currentTile = Tiles[i, j];
+                        Tile currentUser = Tiles[i, j];
 
-                        if (currentTile.TileOwner == User.None)
+                        if (currentUser.TileOwner == User.None)
                         {
-                            validTiles.Add(currentTile);
+                            validTiles.Add(currentUser);
                         }
                     }
                 }
@@ -85,6 +93,7 @@ public class GameManager : MonoBehaviour
         */
 
         // Get the best move that the AI agent can take to try and win the game.
+        Debug.Log("AI thinking...");
         Vector2Int bestMove = GetBestMove();
 
         if (bestMove.x >= 0 && bestMove.y >= 0)
@@ -113,14 +122,14 @@ public class GameManager : MonoBehaviour
                 // Check unowned tiles only.
                 if (boardState[i, j] == User.None)
                 {
+                    // Tempoarily own the tile so that future moves can be simulated based on this board state.
                     boardState[i, j] = User.AI;
-
-                    int score = Minimax(boardState);
+                    int score = Minimax(boardState, false, 0);
 
                     boardState[i, j] = User.None;
 
                     // If the score is the new best, assign it as the new best score and store the indices.
-                    if (score > bestScore)
+                    if (score > bestScore)  
                     {
                         bestScore = score;
                         bestMove = new Vector2Int (i, j);
@@ -132,19 +141,130 @@ public class GameManager : MonoBehaviour
         return bestMove;
     }
 
-    int Minimax(User[,] boardState)
+    /// <summary>
+    /// Taking copy of the board, simulate the score of playing in each space on the board to a given depth of moves using recursion.
+    /// </summary>
+    /// <param name="boardState"></param>
+    /// <param name="isMaximizing"></param>
+    /// <param name="depth"></param>
+    /// <returns></returns>
+    int Minimax(User[,] boardState, bool isMaximizing, int depth)
     {
+        // Get a score of the state of the board for how well the simulated position is for the AI player.
         int result = EvaluateBoard(boardState);
 
+        // If the score is found to not result in a winner, then allow the checks on further tiles.
         if (result != 0)
         {
             return result;
         }
 
+        // If the board is full, end the tile checks.
         if (IsBoardFull(boardState))
         {
             return 0;
         }
+
+        // Limit check for how deep in depth of simulated boards to check for sake of performance.
+        if (depth >= DepthLimit_AI)
+        {
+            return 0;
+        }
+
+        // For checking the cost of the AI move from the AI perspective as to if it is a winning move.
+        if (isMaximizing)
+        {
+            int bestScore = int.MinValue;
+
+            for (int x = 0; x < Width; x++)
+            {
+                for (int y = 0; y < Length; y++)
+                {
+                    if (boardState[x, y] == User.None)
+                    {
+                        // Using recursion to simulate future moves deeper into a given boardstate.
+                        boardState[x, y] = User.AI;
+
+                        int score = Minimax(boardState, false, depth + 1);
+
+                        boardState[x, y] = User.None;
+
+                        bestScore = Mathf.Max(score, bestScore);
+                    }
+                }
+            }
+
+            return bestScore;
+        }
+        else
+        // For checking the cost of the players move from the AI perspective as to if it is a losing move.
+        {
+            int bestScore = int.MaxValue;
+
+            for (int x = 0; x < Width; x++)
+            {
+                for (int y = 0; y < Length; y++)
+                {
+                    if (boardState[x, y] == User.None)
+                    {
+                        // Using recursion to simulate future moves deeper into a given boardstate.
+                        boardState[x, y] = User.Player;
+
+                        int score = Minimax(boardState, true, depth + 1);
+
+                        boardState[x, y] = User.None;
+
+                        bestScore = Mathf.Min(score, bestScore);
+                    }
+                }
+            }
+
+            return bestScore;
+        }
+    }
+
+    // Return a score based on the wehther the board represents a winning or a losing play.
+    int EvaluateBoard(User[,] boardState)
+    {
+        User winner = GetWinner(boardState);
+
+        if (winner == User.AI)
+        {
+            return 10;
+        }
+        else if (winner == User.Player)
+        {
+            return -10;
+        }
+
+        return 0;
+    }
+
+    User GetWinner(User[,] boardState)
+    {
+        User winner = User.None;
+
+        if (winner == User.None)
+        {
+            CheckLines(true, boardState, out winner);
+        }
+
+        if (winner == User.None)
+        {
+            CheckLines(false, boardState, out winner);
+        }
+
+        if (winner == User.None)
+        {
+            CheckDiagonal(0, 0, true, boardState, out winner);
+        }
+
+        if (winner == User.None)
+        {
+            CheckDiagonal(0, 0, false, boardState, out winner);
+        }
+
+        return winner;
     }
 
     bool IsBoardFull(User[,] board)
@@ -289,6 +409,87 @@ public class GameManager : MonoBehaviour
         CurrentPlayer = newPlayer;
     }
 
+    bool CheckLines(bool isColumns, User[,] boardState, out User winner)
+    {
+        User previousTileOwner = User.None;
+        int winnerCount = 0;
+        List<User> winningTiles = new List<User>();
+
+        int i;
+        int j;
+
+        // Checking lines.
+        for (i = 0; DimensionCheckOne(); i++)
+        {
+            for (j = 0; DimensionCheckTwo(); j++)
+            {
+                User currentUser = GetTileIndex(isColumns);
+
+                // currentUser.GetComponentInChildren<UnityEngine.UI.Image>().color = i == 0 ? Color.magenta : j == 0 ? Color.gray : Color.white;
+
+                // Loop over the line and check for player, AI or unowned tiles and track ownership in orders of 3 in a row to find a win.
+                if (currentUser != User.None)
+                {
+                    // If the winning counter is zero, start the count. Also increment counter if the counter is greater then zero and the current tile is same owner as last tile.
+                    if (winnerCount == 0 || winnerCount > 0 && previousTileOwner == currentUser)
+                    {
+                        winnerCount++;
+                        winningTiles.Add(currentUser);
+                    }
+                    // If the previous tile was owned by a different owner the the current, reset the winner to one for the new owners streak.
+                    else
+                    {
+                        winningTiles.Clear();
+                        winnerCount = 1;
+                        winningTiles.Add(currentUser);
+                    }
+
+                    previousTileOwner = currentUser;
+                }
+                else
+                {
+                    // On an unowned tile, any winning tile collection being made is broken therefore reset the winner counts.
+                    winnerCount = 0;
+                    previousTileOwner = User.None;
+                    winningTiles.Clear();
+                    continue;
+                }
+
+                // If a win is found, return the method as true.
+                if (winnerCount == WinningLineAmount)
+                {
+                    winner = currentUser;
+                    return true;
+                }
+            }
+
+            // Resetting the winning counter if no win is found within the current line before moving on to the next.
+            winnerCount = 0;
+            previousTileOwner = User.None;
+            winningTiles.Clear();
+        }
+
+        //  False is returned when no winner is found during the nested for loops.
+        winner = User.None;
+        return false;
+
+        // In method methods.
+        bool DimensionCheckOne()
+        {
+            return isColumns ? i < boardState.GetLength(0) : i < boardState.GetLength(1);
+        }
+
+        bool DimensionCheckTwo()
+        {
+            return isColumns ? j < boardState.GetLength(1) : j < boardState.GetLength(0);
+        }
+
+        User GetTileIndex(bool isColumns)
+        {
+            return isColumns ? boardState[i, j] : boardState[j, i];
+        }
+    }
+
     public bool CheckLines(int firstDimensionLength, int secondDimensionLength, bool isColumns)
     {
         User previousTileOwner = User.None;
@@ -305,7 +506,7 @@ public class GameManager : MonoBehaviour
             {
                 Tile currentTile = GetTileIndex(isColumns);
 
-                // currentTile.GetComponentInChildren<UnityEngine.UI.Image>().color = i == 0 ? Color.magenta : j == 0 ? Color.gray : Color.white;
+                // currentUser.GetComponentInChildren<UnityEngine.UI.Image>().color = i == 0 ? Color.magenta : j == 0 ? Color.gray : Color.white;
 
                 // Loop over the line and check for player, AI or unowned tiles and track ownership in orders of 3 in a row to find a win.
                 if (currentTile.TileOwner != User.None)
@@ -336,9 +537,9 @@ public class GameManager : MonoBehaviour
                 }
 
                 // If a win is found, return the method as true.
-                if (winnerCount == 3)
+                if (winnerCount == WinningLineAmount)
                 {
-                    foreach(Tile tile in winningTiles)
+                    foreach (Tile tile in winningTiles)
                     {
                         tile.GetComponentInChildren<UnityEngine.UI.Image>().color = Color.green;
                     }
@@ -373,6 +574,76 @@ public class GameManager : MonoBehaviour
         }
     }
 
+    bool CheckDiagonal(int startPointIndexOne, int startPointIndexTwo, bool fliped, User[,] boardState, out User winner)
+    {
+        List<User> winningTiles = new List<User>();
+
+        for (int sum = 0; sum < Width + Length - 1; sum++)
+        {
+            User previousTileOwner = User.None;
+            int winnerCount = 0;
+            winningTiles.Clear();
+
+            for (int x = 0; x < Width; x++)
+            {
+                int y = sum - x;
+
+                if (y >= 0 && y < Length)
+                {
+                    int newX = x;
+
+                    if (fliped)
+                    {
+                        newX = (Width - 1) - x;
+                    }
+
+                    User currentUser = boardState[newX, y];
+
+                    // currentUser.GetComponentInChildren<UnityEngine.UI.Image>().color = i == 0 ? Color.magenta : j == 0 ? Color.gray : Color.white;
+
+                    // Loop over the line and check for player, AI or unowned tiles and track ownership in orders of 3 in a row to find a win.
+                    if (currentUser != User.None)
+                    {
+                        // If the winning counter is zero, start the count. Also increment counter if the counter is greater then zero and the current tile is same owner as last tile.
+                        if (winnerCount == 0 || winnerCount > 0 && previousTileOwner == currentUser)
+                        {
+                            winnerCount++;
+                            winningTiles.Add(currentUser);
+                        }
+                        // If the previous tile was owned by a different owner the the current, reset the winner to one for the new owners streak.
+                        else
+                        {
+                            winningTiles.Clear();
+                            winnerCount = 1;
+                            winningTiles.Add(currentUser);
+                        }
+
+                        previousTileOwner = currentUser;
+                    }
+                    else
+                    {
+                        // On an unowned tile, any winning tile collection being made is broken therefore reset the winner counts.
+                        winnerCount = 0;
+                        previousTileOwner = User.None;
+                        winningTiles.Clear();
+                        continue;
+                    }
+
+                    // If a win is found, return the method as true.
+                    if (winnerCount == WinningLineAmount)
+                    {
+                        winner = currentUser;
+                        return true;
+                    }
+
+                }
+            }
+        }
+
+        winner = User.None;
+        return false;
+    }
+
     bool CheckDiagonal(int startPointIndexOne, int startPointIndexTwo, bool fliped)
     {
         List<Tile> winningTiles = new List<Tile>();
@@ -398,7 +669,7 @@ public class GameManager : MonoBehaviour
 
                     Tile currentTile = Tiles[newX, y];
 
-                    // currentTile.GetComponentInChildren<UnityEngine.UI.Image>().color = i == 0 ? Color.magenta : j == 0 ? Color.gray : Color.white;
+                    // currentUser.GetComponentInChildren<UnityEngine.UI.Image>().color = i == 0 ? Color.magenta : j == 0 ? Color.gray : Color.white;
 
                     // Loop over the line and check for player, AI or unowned tiles and track ownership in orders of 3 in a row to find a win.
                     if (currentTile.TileOwner != User.None)
@@ -429,7 +700,7 @@ public class GameManager : MonoBehaviour
                     }
 
                     // If a win is found, return the method as true.
-                    if (winnerCount == 3)
+                    if (winnerCount == WinningLineAmount)
                     {
                         foreach (Tile tile in winningTiles)
                         {
